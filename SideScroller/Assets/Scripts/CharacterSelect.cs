6fdd88be9fe878f24166f6d0706461952b22e625
HiRoS <hiros.neko@gmail.com>
2018-01-24 17:10:03 -0500

    Further Improved Character Selection

--- SideScroller/Assets/Scripts/CharacterSelect.cs
+++ SideScroller/Assets/Scripts/CharacterSelect.cs
@@ -7,18 +7,28 @@ public class CharacterSelect : MonoBehaviour
 {
 
 	[SerializeField] private BearBox[] _bearBoxes;
-
+	[SerializeField] private Light _spotLight;
 
 	[SerializeField] private int _selectedIndex;
 
+	private void Start()
+	{
+		_spotLight.intensity = 0;
+	}
+
 	public void Selected(BearBox selected)
 	{
+		if (_spotLight.intensity == 0)
+		{
+			_spotLight.intensity = 5;
+		}
 		for (int i = 0; i < _bearBoxes.Length; i ++)
 		{
 			if (_bearBoxes[i] == selected)
 			{
 				_bearBoxes[i].Select();
 				_selectedIndex = i;
+				_spotLight.transform.LookAt(_bearBoxes[i].transform.position + .5f*Vector3.up);
 			}
 			else
 			{
