5d4bc1f1b9ed13070d597e71ce6743d3fce263a5
HiRoS <hiros.neko@gmail.com>
2018-01-24 14:12:15 -0500

    Added main menu, start game, and quit buttons Added framework for character selection

---
+++ SideScroller/Assets/CharacterSelect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+
+	[SerializeField] private BearBox[] _bearBoxes;
+
+
+	[SerializeField] private int _selectedIndex;
+
+	public void Selected(BearBox selected)
+	{
+		for (int i = 0; i < _bearBoxes.Length; i ++)
+		{
+			if (_bearBoxes[i] == selected)
+			{
+				_bearBoxes[i].Select();
+				_selectedIndex = i;
+			}
+			else
+			{
+				_bearBoxes[i].Deselect();
+			}
+			
+		}
+	}
+}
