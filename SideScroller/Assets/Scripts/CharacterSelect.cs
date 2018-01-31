574e162fc417d8799eb6a276fd1c0d85f87d008f
HiRoS <hiros.neko@gmail.com>
2018-01-31 17:43:15 -0500

    Update UI in Character select

--- SideScroller/Assets/Scripts/CharacterSelect.cs
+++ SideScroller/Assets/Scripts/CharacterSelect.cs
@@ -5,37 +5,39 @@ using UnityEngine;
 
 public class CharacterSelect : MonoBehaviour
 {
+    [SerializeField] private BearBox[] _bearBoxes;
+    [SerializeField] private Light _spotLight;
 
-	[SerializeField] private BearBox[] _bearBoxes;
-	[SerializeField] private Light _spotLight;
+    [SerializeField] private int _selectedIndex;
 
-	[SerializeField] private int _selectedIndex;
+    private bool _newTarget = false;
 
-	private void Start()
-	{
-		_spotLight.intensity = 0;
-	}
+    [SerializeField, Range(0, 10)] private float _rotateSpeed;
 
-	public void Selected(BearBox selected)
-	{
-		if (_spotLight.intensity == 0)
-		{
-			_spotLight.intensity = 5;
-		}
-		for (int i = 0; i < _bearBoxes.Length; i ++)
-		{
-			if (_bearBoxes[i] == selected)
-			{
-				_bearBoxes[i].Select();
-				_selectedIndex = i;
-				_spotLight.transform.LookAt(_bearBoxes[i].transform.position + .5f*Vector3.up);
-				CharacterPrefs.PlayerBear = i;
-			}
-			else
-			{
-				_bearBoxes[i].Deselect();
-			}
-			
-		}
-	}
+    private void Start()
+    {
+        _spotLight.intensity = 0;
+    }
+
+    public void Selected(BearBox selected)
+    {
+        if (_spotLight.intensity == 0)
+        {
+            _spotLight.intensity = 5;
+        }
+        for (int i = 0; i < _bearBoxes.Length; i++)
+        {
+            if (_bearBoxes[i] == selected)
+            {
+                _bearBoxes[i].Select();
+                _selectedIndex = i;
+                _spotLight.transform.LookAt(_bearBoxes[i].transform.position + .5f * Vector3.up);
+                CharacterPrefs.PlayerBear = i;
+            }
+            else
+            {
+                _bearBoxes[i].Deselect();
+            }
+        }
+    }
 }
