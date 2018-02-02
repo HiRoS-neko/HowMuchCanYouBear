349e556f945d1a4817b258ba7f6d4da346287e59
HiRoS <hiros.neko@gmail.com>
2018-02-01 21:08:58 -0500

    Added framework for hiding Added ledges

---
+++ SideScroller/Assets/Scripts/GrabLedge.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterMotor), typeof(PlayerMove), typeof(Rigidbody))]
+public class GrabLedge : MonoBehaviour
+{
+
+    private CharacterMotor _playerMotor;
+    private PlayerMove _playerMove;
+    private Rigidbody _rigid;
+    private Animator _anim;
+    
+    private void Start()
+    {
+        _playerMotor = GetComponent<CharacterMotor>();
+        _playerMove = GetComponent<PlayerMove>();
+        _rigid = GetComponent<Rigidbody>();
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    public void StartHold(Vector3 pos, int dir)
+    {
+        
+        _playerMove.enabled = false;
+        _playerMotor.enabled = false;
+        _rigid.velocity = Vector3.zero;
+        _rigid.useGravity = false;
+        _anim.SetBool("LedgeGrab", true);
+        this.transform.position = pos - dir*new Vector3(0.65f, 1.2f, 0);
+        
+    }
+
+    public void StopJumpHold()
+    {
+    //player jumped while holding... big jump
+        _playerMove.enabled = true;
+        _playerMotor.enabled = true;
+        _rigid.useGravity = true;
+        _anim.SetBool("LedgeGrab", false);
+        
+        _playerMove.Jump(Vector3.up*25);
+
+    }
+
+    public void StopHold()
+    {
+        
+        //player let go drop hhim
+        _playerMove.enabled = true;
+        _playerMotor.enabled = true;        
+        _rigid.useGravity = true;
+        _anim.SetBool("LedgeGrab", false);
+        _anim.SetTrigger("Falling");
+
+
+
+    }
+}
---
+++ SideScroller/Assets/Scripts/LedgeObject.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class LedgeObject : MonoBehaviour
+{
+    private Collider _trigger;
+    private bool _hanging;
+
+    [SerializeField, Range(-1, 1)] private int _direction;
+
+    private void Start()
+    {
+        _trigger = GetComponent<Collider>();
+
+        if (_trigger.isTrigger == false)
+        {
+            Debug.Log("Collider on " + name + " must be a collider");
+            _trigger.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (_hanging == false && other.gameObject.CompareTag("Player") && Input.GetAxis("Interact") == 1)
+        {
+            _hanging = true;
+            other.GetComponent<GrabLedge>().StartHold(this.transform.position, _direction);
+        }
+        else if (_hanging && other.gameObject.CompareTag("Player") && Input.GetAxis("Jump") == 1)
+        {
+            _hanging = false;
+            other.GetComponent<GrabLedge>().StopJumpHold();
+        }
+        else if (_hanging && other.gameObject.CompareTag("Player") && Input.GetAxis("Interact") == 0)
+        {
+            _hanging = false;
+            other.GetComponent<GrabLedge>().StopHold();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_hanging && other.gameObject.CompareTag("Player"))
+        {
+            _hanging = false;
+            other.GetComponent<GrabLedge>().StopHold();
+        }
+    }
+}
