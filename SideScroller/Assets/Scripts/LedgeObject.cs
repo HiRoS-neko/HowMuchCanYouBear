3fe617f568cf07edb74094f6bd48aec5524eab88
HiRoS <hiros.neko@gmail.com>
2018-02-13 19:46:10 -0500

    Added Closet Fixed Jump a little, should be a little less buggy

--- SideScroller/Assets/Scripts/LedgeObject.cs
+++ SideScroller/Assets/Scripts/LedgeObject.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Text;
+using UnityEngine;
+using UnityEngine.Collections;
 
 [RequireComponent(typeof(Collider))]
 public class LedgeObject : MonoBehaviour
@@ -11,6 +13,7 @@ public class LedgeObject : MonoBehaviour
 
     private Collider _trigger;
     private bool _hanging;
+    private bool _jump;
 
     [SerializeField] private Direction _direction;
 
@@ -27,25 +30,28 @@ public class LedgeObject : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (_hanging == false && other.gameObject.CompareTag("Player") && Input.GetAxis("Interact") == 1)
+        if (!_jump && _hanging == false && other.gameObject.CompareTag("Player") && Input.GetAxis("Interact") == 1 &&
+            Input.GetAxis("Jump") == 0)
         {
             _hanging = true;
             switch (_direction)
             {
                 case Direction.left:
-                    other.GetComponent<GrabLedge>().StartHold(this.transform.position - new Vector3(0.65f, 1.2f, 0), false);
+                    other.GetComponent<GrabLedge>()
+                        .StartHold(this.transform.position - new Vector3(0.65f, 1.2f, 0), false);
                     break;
                 case Direction.right:
-                    other.GetComponent<GrabLedge>().StartHold(this.transform.position - new Vector3(-0.65f, 1.2f, -0), true);
+                    other.GetComponent<GrabLedge>()
+                        .StartHold(this.transform.position - new Vector3(-0.65f, 1.2f, -0), true);
                     break;
             }
-            
         }
-        else if (_hanging && other.gameObject.CompareTag("Player") && Input.GetAxis("Jump") == 1)
+        else if (!_jump && _hanging && other.gameObject.CompareTag("Player") && Input.GetAxis("Jump") == 1)
         {
+            _jump = true;
             other.GetComponent<GrabLedge>().StopJumpHold();
         }
-        else if (_hanging && other.gameObject.CompareTag("Player") && Input.GetAxis("Interact") == 0)
+        else if (!_jump && _hanging && other.gameObject.CompareTag("Player") && Input.GetAxis("Interact") == 0)
         {
             _hanging = false;
             other.GetComponent<GrabLedge>().StopHold();
@@ -57,7 +63,11 @@ public class LedgeObject : MonoBehaviour
         if (_hanging && other.gameObject.CompareTag("Player"))
         {
             _hanging = false;
-            other.GetComponent<GrabLedge>().StopHold();
+            if (!_jump)
+            {
+                other.GetComponent<GrabLedge>().StopHold();
+            }
+            else _jump = false;
         }
     }
 }
